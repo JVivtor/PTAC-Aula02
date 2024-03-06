8f5880f0671a79b2449c55b0b52f14a57fab4e15
JVivtor <joaovictorsilvaalmeida3@gmail.com>
2024-03-06 15:59:19 -0400

    06/03/24 aula

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Footer from "./Components/Footer "; 
+import "./globals.css"
 
  export default function App() {
 
@@ -14,4 +15,4 @@ import Footer from "./Components/Footer ";
 
  ); 
 
-}
+ }
